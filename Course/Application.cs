12fdb7701f6a2029d03f8efe642a9d411b6c3568
SIIPOLSKA\snowinski <snowinski@pl.sii.eu>
2019-12-20 16:57:16 +0100

    git ignore and config

--- Course/Application.cs
+++ Course/Application.cs
@@ -1,16 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Configuration;
 
 namespace Course
 {
     public class Application
     {
         private WelcomeExercise _exercise;
+
         public Application()
         {
             _exercise = new WelcomeExercise();
+            GetConfiguration();
             RunExercise();
+        }
+
+        private void GetConfiguration()
+        {
+            var sth = ConfigurationManager.AppSettings["Name"];
         }
 
         private void RunExercise()
@@ -18,9 +24,9 @@ namespace Course
             _exercise.VariableCreate();
             _exercise.ArrayCreate();
             _exercise.ColectionCreate();
-            _exercise.AddOneToColection(new List<int>{1,2,3});
+            _exercise.AddOneToColection(new List<int> { 1, 2, 3 });
             _exercise.Split("ala ma kota");
-            _exercise.Substring("ala ma kota",3,2);
+            _exercise.Substring("ala ma kota", 3, 2);
         }
     }
 }
